8c2d5aba2dfd57656fe842ecb59f98f1ef1e35aa
cg8 <5712.cg8@gmail.com>
2025-08-30 18:29:22 +0800

    feat: enhance homework due date display
    - Update due date display to show formatted date if not today
    - Add isToday function to check if the due date is today
    - Improve error handling message for fetching homework
    
    This commit enhances the user experience by providing a clearer
    indication of homework due dates. If the due date is not today,
    it will now display the formatted date. Additionally, the error
    handling message has been simplified for better clarity.

--- src/components/homework/homework.vue
+++ src/components/homework/homework.vue
@@ -1,4 +1,3 @@
-<!-- src/views/Homework.vue -->
 <template>
   <div class="p-4">
     <!-- 加载中 -->
@@ -15,11 +14,15 @@
     <div v-else-if="homework" class="space-y-2">
       <h2 class="text-xl font-semibold">
         {{ homework.class_name }}
-        <span class="ml-2 text-gray-500 text-sm">
-          截止 {{ formatYYYYMMDD(homework.due_date) }}
+
+        <!-- 判断是否是今天，如果不是今天，显示截止日期 -->
+        <span class="ml-2 text-gray-500 text-sm" v-if="!isToday(homework.due_date)">
+          截止 {{ formatYYYYMMDDToDate(homework.due_date) }}
         </span>
       </h2>
-        <div class="homework-content" v-html="formattedContent"></div>
+
+      <!-- 显示作业内容 -->
+      <div class="homework-content" v-html="formattedContent"></div>
     </div>
 
     <!-- 没有数据 -->
@@ -32,25 +35,39 @@
 <script setup>
 import { ref, onMounted, computed } from 'vue'
 import HomeworkService from '@/services/basic/homework'
-import formatYYYYMMDD from '@/utils/formatDate'
+import formatYYYYMMDDToDate from '@/utils/formatDate'
 
 const homework = ref(null)
 const loading = ref(true)
 const error = ref(null)
 let formattedContent = ''
 
+// 判断日期是否是今天
+const isToday = (dueDate) => {
+  const today = new Date()
+
+  // 将日期格式化为 'YYYY-MM-DD'，方便比较
+  const todayFormatted = today.toISOString().split('T')[0].replace(/-/g, '')
+  const dueDateFormatted = homework.value.due_date.toString()
+
+  console.log(todayFormatted, dueDateFormatted)
+  console.log(todayFormatted===dueDateFormatted)
+
+  return todayFormatted === dueDateFormatted
+}
+
 onMounted(async () => {
   try {
     const res = await HomeworkService.getTodayHomework(1)
     homework.value = res.data.data
 
+    // 格式化作业内容，处理换行符
     formattedContent = computed(() => {
-      console.log(homework.value.homework_content)
       return homework.value.homework_content.replace(/\n/g, '<br>')
     })
 
   } catch (err) {
-    error.value = err.message || '获取作业失败'
+    error.value = err.message || '失败'
   } finally {
     loading.value = false
   }
